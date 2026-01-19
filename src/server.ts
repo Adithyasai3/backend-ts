01443d795c568db09d779eae23333eed1adc2692
Adithyasai3 <adithya.asg@gmail.com>
2026-01-19 23:36:44 +0530

    server logic changed

--- src/server.ts
+++ src/server.ts
@@ -16,9 +16,6 @@ let nextId = 1;
 app.get("/hello",(req,res)=>{
   res.json({status:"Hello"});
 })
-app.get("/hello",(req,res)=>{
-  res.json({status:"Hello"});
-})
 
 
 app.post("/users",(req,res)=>{
